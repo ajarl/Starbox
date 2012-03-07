fff3dede5b77b4c0d822db181e8883b3d0b741a8
Anders Jarleberg <brewte@gmail.com>
2012-03-07 16:10:43 +0100

    Finns util-paket

---
+++ src/se/starbox/util/Example.java
@@ -0,0 +1,9 @@
+package se.starbox.util;
+
+/**
+ * Denna klass är bara här för att illustrera att se.starbox.util används för andra klasser som inte är modeller/controllers.
+ * Ta bort denna efter att det finns riktiga klasser i detta paket.
+ */
+public class Example {
+
+}
