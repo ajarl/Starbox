d1dc0f6e8e89607298a267406749ffaf2bd84c2a
unknown <Anders@Anders-laptop.(none)>
2012-03-07 15:06:40 +0100

    Initial commit

---
+++ WebContent/view.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>You set param1</title>
+</head>
+<body>
+Hello, you set param1 to <%= request.getParameter("param1") %>.
+</body>
+</html>
---
+++ src/se/starbox/controllers/SearchController.java
@@ -0,0 +1,58 @@
+package se.starbox.controllers;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchController
+ */
+@WebServlet(
+		description = "Handles search requests", 
+		urlPatterns = { 
+				"/SearchController", 
+				"/", 
+				"/query"
+		})
+public class SearchController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Map<String, String[]> params = request.getParameterMap();
+		String viewStr="";
+		if (params.containsKey("param1")) {
+			viewStr = "/view.jsp";
+		} else {
+			viewStr = "/noview.jsp";
+		}
+		RequestDispatcher view = request.getRequestDispatcher(viewStr);
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
