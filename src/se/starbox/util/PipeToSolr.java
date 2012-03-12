cc7a138bd547202d34352e315082c47be8a10af6
Linus Eriksson <zvebbo@hotmail.com>
2012-03-12 12:51:17 +0100

    PipeToSolr och SearchModel.

---
+++ src/se/starbox/util/PipeToSolr.java
@@ -0,0 +1,40 @@
+
+/**
+ * A class made as a bridge between OpenPipeline and Solr. PipeToSolr takes the
+ * output from OpenPipeline and sends it to Solr.
+*
+* @author Linus, Robin
+*
+*/
+public class PipeToSolr {
+	
+	public PipeToSolr(){
+		initialize();
+	}
+
+	/**
+	 * Initialises.
+	 */
+	private void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Takes input from OpenPipeline and translates it to xml. 
+	 * Compiles
+	 * @param item
+	 */
+	private void processItem(Item item){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void printToFile(List<Item>){
+		
+	}
+	
+	
+}
