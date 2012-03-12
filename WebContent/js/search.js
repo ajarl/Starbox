130a777602adc0c5b447044e92cfa39b9afbdbe9
Otto Nordgren <otto.nordgren@armada.nu>
2012-03-12 19:35:34 +0100

    fixed bug in filters

--- WebContent/js/search.js
+++ WebContent/js/search.js
@@ -1,29 +1,31 @@
 $(document).ready(function() {
+	
+	$('#search-query').focus();
+	
+	$('#search-query').keyup(function() {
+		$.get('/starbox/search/', {
+			query : $(this).val()
+		}, function(data) {
 
-  $('#search-query').keyup(function() {
-    $.get('/starbox/search/', { query: $(this).val() }, 
-      function(data) {
-    
-    });
-  });
-  
-  $('.search-filters h2').click(function() {
-	  $(this).next().toggle();
-	  $(this).toggleClass('open');
-	  
-	  if($(this).hasClass('open'))
-	  {
-		  $(this).find('span').html('[');
-	  } else {
-		  $(this).find('span').html(']');
-	  }
-	  
-	  return false;
-  });
-  
-  $('.search-filters li').click(function() {
-	  $(this).toggleClass('selected');
+		});
+	});
 
-	  return false;
-  });
+	$('.search-filters h2').click(function() {
+		$(this).next().toggle();
+		$(this).toggleClass('open');
+
+		if ($(this).hasClass('open')) {
+			$(this).find('span').html('[');
+		} else {
+			$(this).find('span').html(']');
+		}
+
+		return false;
+	});
+
+	$('.search-filters li').click(function() {
+		$(this).toggleClass('selected');
+
+		return false;
+	});
 });
