eacb8c151eff05204cfa79fb14767c6472dc63fb
Kim Nilsson <droogie.kim@gmail.com>
2012-04-11 11:00:37 +0200

    jobbat med solr

--- src/se/starbox/models/SearchModel.java
+++ src/se/starbox/models/SearchModel.java
@@ -1,6 +1,9 @@
 package se.starbox.models;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
@@ -8,8 +11,13 @@ import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.request.DirectXmlRequest;
 import org.apache.solr.core.CoreContainer;
+import org.apache.solr.schema.UUIDField;
 import org.xml.sax.SAXException;
 
 
@@ -20,12 +28,13 @@ import org.xml.sax.SAXException;
 * @author Linus, Robin, Kim
 *
 */
-
 public class SearchModel {
 
+	// Handle for Solr
+	private static SolrServer solr = null;
 	
-	private static EmbeddedSolrServer solr;
-	private final String SOLR_HOME = "solr/";
+	// URI to Solr
+	private String SOLR_URI = "http://localhost:8080/starbox-solr-server/";
 
 	/**
 	* Initiate the model instance. Creates an instance of Solr on startup if
@@ -34,63 +43,84 @@ public class SearchModel {
 	* @throws MalformedURLException
 	*/
 	public SearchModel() {
-	
 		// Initilize Solr
-//		if (solr == null)
-			// detta krashar sidan
-//			getSolr();
-//		
-//			
-//		try {
-//			solr.deleteById("*");
-//		} catch (SolrServerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	
+		if (solr == null)
+			getSolr();
+		
+		//checkConnection();
+		testFill();
 	}
-	
-	
-	/**
-	 * This method creates an embedded instance of Solr.
-	 */
-	private void getSolr() {
-		File home = new File(SOLR_HOME);
-		File f = new File(home, "conf/solr.xml");
-/*
-		CoreContainer container = new CoreContainer();
+
+	// Fill the db with test data.
+	public void testFill() {
+		File testIndexData = new File("C:/Users/Kim/workspace/Starbox/exempelIndexData.xml");
+		
+		String doc = getStringFromDocument(testIndexData);
+		System.out.println("Read file, content was: " + doc);
+		
+		DirectXmlRequest xmlreq = new DirectXmlRequest("/update", doc);
 		try {
-			container.load(SOLR_HOME, f);
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+			solr.request(xmlreq);
+		} catch (SolrServerException | IOException e) {
+			System.err.println("Error updating solr");
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
+		}
+	}
+
+
+	/**
+	 * Reads a file into a string.
+	 * @param file
+	 * @return a string with the content of file
+	 */
+	private String getStringFromDocument(File file) {
+		BufferedReader br = null;
+	
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException fe) {
+			System.err.println("Error! Couldnt find file " + file.getName());
+			fe.printStackTrace();
+		}
+		
+		String line;
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			while((line=br.readLine()) != null)
+				sb.append(line);
+		} catch(IOException e) {
+			System.err.println("Error reading line.");
 			e.printStackTrace();
 		}
 		
-		solr  = new EmbeddedSolrServer(container,
-				"core");
-		/*
-		try {
-			System.setProperty("solr.solr.home", SOLR_HOME);
-		} catch (SecurityException se) {
-			System.err.println("SearchModel caught a SecurityException. Couldn't set system property!");
-			se.toString();
-		}
-	
-		CoreContainer coreContainer = new CoreContainer();
-		CoreContainer.Initializer initiliazer = new CoreContainer.Initializer();
-		solr = new EmbeddedSolrServer(coreContainer, "core"); */
-		*/
+		return sb.toString();
 	}
 
+	private void checkConnection() {
+		try {
+			solr.ping();
+		} catch (SolrServerException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * This method creates an embedded instance of Solr.
+	 * If theres an exception solr will be set to <b>null</b>.
+	 */
+	private void getSolr() {
+		try {
+			solr = new CommonsHttpSolrServer(SOLR_URI);
+		} catch(MalformedURLException mex) {
+			System.err.println("Error (MalformedURLException) in SearchModel. Here is the stack trace:");
+			mex.printStackTrace();
+			solr = null;
+		}
+	}
 
 
 	/**
