09f14b98b57573d84a54972338e948aefe46a1e4
Otto Nordgren <otto.nordgren@armada.nu>
2012-05-08 09:29:18 +0200

    added functionality for shutdown/index buttons in settings

--- WebContent/js/settings.js
+++ WebContent/js/settings.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-	
+	$('#update-index').click(function() {
+		$.post('/starbox/settings', 'updateindex=true', function() {
+			
+		});
+	});
+	$('#shutdown').click(function() {
+		$.post('/starbox/settings', 'shutdown=true', function() {
+			
+		});
+	});
 	$('.save-setting').click(function() {
 		var button = $(this);
 		var data = $(this).parent().prev('td').children('input').serialize();
 		$(this).next('span').css('color', '#666').text('Saving');
 
-		$.post("/starbox/settings", data, function(){
+		$.post('/starbox/settings', data, function(){
 			$(button).next('span').css('color', 'green').text('Saved');
 	    })
 	    .error(function() { 
